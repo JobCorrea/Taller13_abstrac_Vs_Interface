54ee246160f790fd9da7f7a41e269ab5cc0d1f5d
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-20 14:33:50 -0500

    Prubas de Objetos -Figura-

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
        
-        
+    Figura rectangulo = new Rectangulo(8, 4);
+        Figura triangulo = new Triangulo(6, 3);
+
+        rectangulo.mostrarArea();
+        triangulo.mostrarArea();
+        System.out.println("---------------------");
 
         
     }
